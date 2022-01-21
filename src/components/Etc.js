280368bb2620ec99eea847c078154a137e9a81f1
Eric Dao <eric_dao@hotmail.com>
2022-01-20 23:31:03 -0800

    create projects and etc page

--- src/components/Etc.js
+++ src/components/Etc.js
@@ -4,7 +4,105 @@ import "./Etc.css";
 export default function Etc() {
   return (
     <>
-      <div className="etc-container"></div>
+      <div className="etc-container">
+        <div className="skills-header">
+          <h1>Skills</h1>
+        </div>
+        <div className="skills-section">
+          <ul className="skills-icons">
+            <li>
+              <img src="./images/htmlicon.png" className="html-icon"></img>
+              <span>HTML</span>
+            </li>
+            <li>
+              <img src="./images/cssicon.png" className="html-icon"></img>
+              <span>CSS</span>
+            </li>
+            <li>
+              <img
+                src="./images/javascripticon.png"
+                className="javascript-icon"
+              ></img>
+              <span>JavaScript</span>
+            </li>
+            <li>
+              <img src="./images/reacticon.png" className="react-icon"></img>
+              <span>React</span>
+            </li>
+            <li>
+              <img src="./images/vueicon.png" className="vue-icon"></img>
+              <span>Vue</span>
+            </li>
+            <li>
+              <img src="./images/pythonicon.png" className="python-icon"></img>
+              <span>Python</span>
+            </li>
+            <li>
+              <img src="./images/javaicon.png" className="java-icon"></img>
+              <span>Java</span>
+            </li>
+            <li>
+              <img src="./images/cicon.png" className="c-icon"></img>
+              <span>C++</span>
+            </li>
+            <li>
+              <img src="./images/sqlicon.png" className="sql-icon"></img>
+              <span>MySQL</span>
+            </li>
+            <li>
+              <img src="./images/giticon.png" className="git-icon"></img>
+              <span>Git</span>
+            </li>
+          </ul>
+        </div>
+        <div className="listen-header">
+          <h1>Currently Listening</h1>
+        </div>
+        <div className="row">
+          <div className="listen-item">
+            <a
+              href="https://open.spotify.com/album/2nLOHgzXzwFEpl62zAgCEC"
+              target="_blank"
+            >
+              <img src="./images/dawnfm.jpg" className="listen-img"></img>
+            </a>
+          </div>
+          <div className="listen-item">
+            <a
+              href="https://open.spotify.com/album/4oxmme6i4mypSt2DDzPTsW"
+              target="_blank"
+            >
+              <img src="./images/ds4.jpg" className="listen-img"></img>
+            </a>
+          </div>
+          <div className="listen-item">
+            <a
+              href="https://open.spotify.com/album/3N3alcn9EvmanafUt70Vxh"
+              target="_blank"
+            >
+              <img src="./images/fmbev.jpg" className="listen-img"></img>
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="listen-item">
+            <a
+              href="https://open.spotify.com/album/3r46DPIQeBQbjvjjV5mXGg"
+              target="_blank"
+            >
+              <img src="./images/melodicblue.jpeg" className="listen-img"></img>
+            </a>
+          </div>
+          <div className="listen-item">
+            <a
+              href="https://open.spotify.com/album/2WmJ5wp5wKBlIJE6FDAIBJ"
+              target="_blank"
+            >
+              <img src="./images/lifeofdon.jpeg" className="listen-img"></img>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,5 +2,76 @@ import React from "react";
 import "./Projects.css";
 
 export default function Projects() {
-  return <></>;
+  return (
+    <>
+      <div className="projects-header">
+        <h1>Projects</h1>
+      </div>
+      <div className="projects-container">
+        <div className="row">
+          <div className="project-item">
+            <img src="./images/card-game.png"></img>
+            <div className="project-description">
+              <div className="img-description">
+                <p>
+                  <b>Matching Pairs</b>
+                  <p>
+                    A Java based memory game where the user must match all pairs
+                    of cards in order to win.{" "}
+                  </p>
+                  <br></br>
+                  <i>Java</i>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="project-item">
+            <img src="./images/basketball-player.png"></img>
+            <div className="project-description">
+              <div className="img-description">
+                <p>
+                  <b>StatFinder</b>
+                  <p>
+                    A React based web application that retrieves data from an
+                    API and displays the 2020 NBA Orlando bubble player data.{" "}
+                  </p>
+                  <br></br>
+                  <i>JavaScript / React / HTML/ CSS</i>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="project-item">
+            <img src="./images/wordsearch.png"></img>
+            <div className="project-description">
+              <div className="img-description">
+                <p>
+                  <b>WordMe</b>
+                  <p>
+                    A JavaScript based Discord chatbot with real-time response
+                    and command for users.{" "}
+                  </p>
+                  <br></br>
+                  <i>JavaScript</i>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="project-item">
+            <img src="./images/html.png"></img>
+            <div className="project-description">
+              <div className="img-description">
+                <p>
+                  <b>Personal Website</b>
+                  <p>A personal website created using React.</p>
+                  <br></br>
+                  <i>JavaScript / React / HTML / CSS</i>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
